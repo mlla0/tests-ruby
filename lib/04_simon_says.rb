e662bb37f41cf53eac1e38d0b72a8bc66153b370
Antoine Vercoutère <antoine@mlav.land>
2019-01-14 13:05:59 +0100

    Ajout de l'exo 04_simon-says terminé

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,4 +1,28 @@
 def echo(word)
-	return "word"
+	return "#{word}"
 end
 
+def shout(word)
+	return "#{word.upcase}"
+end
+
+def repeat(word, i = 2)
+	return [word] * i * " "
+end
+
+def start_of_word(word, a)
+	return word[0..a-1]
+end
+
+def first_word(word)
+	word.split.first
+end
+
+def titleize(string)
+	special_words = ["the", "and"]
+	words = []
+	string.split.each_with_index do |w, i|
+	i > 0 && special_words.include?(w) ? words << w : words << w.capitalize
+	end
+	words.join(" ")
+end
