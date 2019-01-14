8d9af62224fa6e2b3b582b2a4c1ca565081e90dc
Antoine Vercoutère <antoine@mlav.land>
2019-01-14 17:53:54 +0100

    Ajout de méthodes dans exo 6

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,15 +1,44 @@
 def translate(string)
 	case 
 
-	when string.start_with?(/[aeiou]/) 
+	when string.start_with?(/[aeiou]/) && string[1] =~ /[^aeiou]/
+		and_ay(string)
+
+	when string[0..1] =~ /qu/
+		string = string[2...string.length] + string[0..1]
+		and_ay(string)
+
+	when string[0] =~ /[^aeiou]/ && string =~ /qu/
+		string = string[3...string.length] + string[0..2]
 		and_ay(string)
 
 	when string[0] =~ /[^aeiou]/ && string[1] =~ /[aeiou]/
 		string = string.reverse
 		and_ay(string)
 
-	when string[0] =~ /[^aeiou]/ && string[1] =~ /[^aeiou]/
+	when string[0..1] =~ /[^aeiou]/ && string[2] =~ /[aeiou]/
 		string = string[2...string.length] + string[0..1]
+		and_ay(string)
+
+	when string.split.count == 2
+		string = string.split
+		string = string[0] + " " + string[1].slice(1,2) + string[1].slice(0)
+		and_ay(string)
+
+	when string[0..2] =~ /[^aeiou]/ 
+		string = string[3...string.length] + string[0..2]
+		and_ay(string)
+
+	when string =~ /sch/ 
+		string = string[3...string.length] + string[0..2]
+		and_ay(string)
+
+	when string.split.count > 2
+		string = string.split
+		string.each do |w|
+			w = w.slice(2,6) + w.slice(0,1)
+		end
+		string = string.join(" ")
 		and_ay(string)
 
 	end
